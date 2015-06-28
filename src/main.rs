262daf44d9478a70fc8d945278fd908a0b79fbe7
Nick Massey <nickmass@nickmass.com>
2015-06-28 15:28:24 -0600

    Fix whitespace

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,24 @@ use openssl::ssl::*;
 mod irc;
 
 fn main() {
-	let connection_result = TcpStream::connect("127.0.0.1:6697");
-	if connection_result.is_err() {
-		return;
-	}
+    let connection_result = TcpStream::connect("127.0.0.1:6697");
+    if connection_result.is_err() {
+        return;
+    }
 
-	let connection = connection_result.unwrap();
-	let ssl_context = SslContext::new(SslMethod::Sslv23);
-	let ssl_stream = SslStream::new(&ssl_context.unwrap(), connection).unwrap();
-	let reader_connection = ssl_stream.try_clone().unwrap();
-	let reader = IrcReader::new(reader_connection);
-	let writer_connection = ssl_stream.try_clone().unwrap();
-	let mut writer = IrcWriter::new(writer_connection);
+    let connection = connection_result.unwrap();
+    let ssl_context = SslContext::new(SslMethod::Sslv23);
+    let ssl_stream = SslStream::new(&ssl_context.unwrap(), connection).unwrap();
+    let reader_connection = ssl_stream.try_clone().unwrap();
+    let reader = IrcReader::new(reader_connection);
+    let writer_connection = ssl_stream.try_clone().unwrap();
+    let mut writer = IrcWriter::new(writer_connection);
 
-	thread::spawn(move || {
-		let stdin = io::stdin();
-		let stdin_lock = stdin.lock();
-		for in_line in stdin_lock.lines() {
-			let _ = in_line.and_then(|x| {
+    thread::spawn(move || {
+        let stdin = io::stdin();
+        let stdin_lock = stdin.lock();
+        for in_line in stdin_lock.lines() {
+            let _ = in_line.and_then(|x| {
 
                 match str::from_utf8(x.as_bytes()).unwrap() {
                     "user" => {
@@ -44,20 +44,20 @@ fn main() {
                         writer.write(b"\r\n");
                     }
                 }
-				Ok(x)
-			});
-		}
-	});
+                Ok(x)
+            });
+        }
+    });
 
-	for irc_msg in reader {
-		parse_command(&irc_msg.message());
-	}
+    for irc_msg in reader {
+        parse_command(&irc_msg.message());
+    }
 }
 
 fn parse_command(command: &[u8]) {
-	let command_str = str::from_utf8(command);
-	if command_str.is_err() {
-		return;
-	}
-	println!("{}", command_str.unwrap());
+    let command_str = str::from_utf8(command);
+    if command_str.is_err() {
+        return;
+    }
+    println!("{}", command_str.unwrap());
 }
